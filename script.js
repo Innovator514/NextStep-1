13c3f9c2eeeb4200023734820e0ab1ae3470e9b2
emily-1234567 <emilysmile324@gmail.com>
2025-11-17 08:54:43 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-const hamMenu = document.querySelector('.hamburger-menu');
 
-const offScreenMenu = document.querySelector('.off-screen-menu');
-
-hamMenu.addEventListener('click', () => {
-  hamMenu.classList.toggle('active');
-  offScreenMenu.classList.toggle('active');
-})
